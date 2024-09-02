8d0e348158a6ff4dec25d6c85e556ab29bfa39ad
Dthj21 <diegothj57@gmail.com>
2024-09-02 17:57:51 -0600

    add work_experience branch #2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { WorlExperienceComponent } from './worl-experience/worl-experience.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, WorlExperienceComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
