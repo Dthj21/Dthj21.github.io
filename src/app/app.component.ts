21c358d3337d94cec110166eea30b6679b9dabb6
Dthj21 <diegothj57@gmail.com>
2024-09-02 18:17:08 -0600

    add education branch #3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { WorlExperienceComponent } from './worl-experience/worl-experience.component';
+import { EducationComponent } from './education/education.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, WorlExperienceComponent],
+  imports: [RouterOutlet, HeaderComponent, WorlExperienceComponent, EducationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
