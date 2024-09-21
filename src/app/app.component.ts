08888a58a73a558c76bae78fbc0372451acabab1
Diego Tristán Hernández Jiménez <127570586+Dthj21@users.noreply.github.com>
2024-09-21 11:39:23 -0600

    create skills branch #4 (#22)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { WorlExperienceComponent } from './worl-experience/worl-experience.component';
 import { EducationComponent } from './education/education.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, WorlExperienceComponent, EducationComponent],
+  imports: [RouterOutlet, HeaderComponent, WorlExperienceComponent, EducationComponent, SkillsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
