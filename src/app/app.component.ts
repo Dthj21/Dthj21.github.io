3db703336603d8f2ecc7fdaf30404349250d66d4
Diego Tristán Hernández Jiménez <127570586+Dthj21@users.noreply.github.com>
2024-09-21 13:39:55 -0600

    add languages branch #5 (#23)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { HeaderComponent } from './header/header.component';
 import { WorlExperienceComponent } from './worl-experience/worl-experience.component';
 import { EducationComponent } from './education/education.component';
 import { SkillsComponent } from './skills/skills.component';
+import { LanguagesComponent } from './languages/languages.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, WorlExperienceComponent, EducationComponent, SkillsComponent],
+  imports: [RouterOutlet, HeaderComponent, WorlExperienceComponent, EducationComponent, SkillsComponent, LanguagesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
